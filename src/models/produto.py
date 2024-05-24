62be530afa22d1d8a896734f33d8bf66da9421b8
ALUNO <aluno@ifsp>
2024-05-23 22:24:29 -0300

    23/05/24- criação do produto models e produto forms

---
+++ src/models/produto.py
@@ -0,0 +1,20 @@
+import uuid
+from sqlalchemy import ForeignKey, Uuid, String, DECIMAL, Integer, Boolean, Text
+from sqlalchemy.orm import mapped_column, relationship
+from src.modules import db
+from src.models.base_mixin import BasicRepositoryMixin, TimeStampMixin
+
+
+class Produto(db.Model, BasicRepositoryMixin, TimeStampMixin):
+    __tablename__ = 'produtos'
+    id = mapped_column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome = mapped_column(String(100), nullable=False, index=True)
+    preco = mapped_column(DECIMAL(10,2), default=0.00, nullable=False)
+    estoque = mapped_column(Integer, default=0)
+    ativo = mapped_column(Boolean, default=True, nullable=False)
+    possui_foto = mapped_column(Boolean, default=False, nullable=False)
+    foto_base64 = mapped_column(Text, default=None, nullable=True)
+    foto_mine = mapped_column(String(64), nullable=True, default=None)
+    categoria_id = mapped_column(Uuid(as_uuid=True), ForeignKey('categoria.id'))
+
+    categoria = relationship('Categoria', back_populates='lista_de_produtos')
