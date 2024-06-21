08d80141992e47c1d2085df786804696b0f09562
ALUNO <aluno@ifsp>
2024-06-20 22:00:46 -0300

    .

--- src/routes/produto.py
+++ src/routes/produto.py
@@ -108,8 +108,13 @@ def delete(produto_id):
 def lista():
     page = request.args.get('page', type=int, default=1)
     pp = request.args.get('pp', type=int, default=25)
+    q = request.args.get('q', type=str, default="")
 
     sentenca = db.select(Produto).order_by(Produto.nome)
+
+    if q != "":
+        sentenca = sentenca.filter(Produto.nome.ilike(f"%{q}%"))
+
     try:
         rset = db.paginate(sentenca, page=page, per_page=pp)
     except NotFound:
@@ -123,7 +128,8 @@ def lista():
                            title="Lista de produtos",
                            rset=rset,
                            page=page,
-                           pp=pp)
+                           pp=pp,
+                           q=q)
 
 
 
